21289168f7e3006875a5c0279e49a3ab7afca2cc
Janek Mann <janek.mann@knaufenergy.com>
2022-10-04 17:01:55 +0100

    turn on center crops

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -39,7 +39,7 @@ class PersonalizedBase(Dataset):
                  set="train",
                  placeholder_token="dog",
                  per_image_tokens=False,
-                 center_crop=False,
+                 center_crop=True,
                  mixing_prob=0.25,
                  coarse_class_text=None,
                  reg=False
