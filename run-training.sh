44d000e285cb5b140f08625bc07edcabb14421df
Janek Mann <janek.mann@knaufenergy.com>
2022-10-07 17:40:27 +0100

    wlop

--- run-training.sh
+++ run-training.sh
@@ -1,15 +1,12 @@
-git clone https://github.com/djbielejeski/Stable-Diffusion-Regularization-Images-person_ddim.git
-mkdir -p regularization_images/person_ddim
-mv -v Stable-Diffusion-Regularization-Images-person_ddim/person_ddim/* regularization_images/person_ddim/   
 python "main.py" \
  --base configs/stable-diffusion/v1-finetune_unfrozen.yaml \
  -t \
- --actual_resume "dan.ckpt" \
+ --actual_resume "alina.ckpt" \
  --reg_data_root "/workspace/Dreambooth-Stable-Diffusion/regularization_images/person_ddim" \
- -n "alina" \
+ -n "wlop" \
  --gpus 0, \
  --data_root "/workspace/Dreambooth-Stable-Diffusion/training_samples" \
- --max_training_steps 2000 \
- --class_word "girl" \
- --token "alina" \
+ --max_training_steps 3200 \
+ --class_word "style" \
+ --token "wlop" \
  --no-test
