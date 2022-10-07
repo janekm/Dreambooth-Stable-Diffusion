b329ab2ad347a5e9c1030c58a3da519f49868882
Janek Mann <janek.mann@knaufenergy.com>
2022-10-07 14:24:14 +0100

    alina

--- run-training.sh
+++ run-training.sh
@@ -1,12 +1,15 @@
+git clone https://github.com/djbielejeski/Stable-Diffusion-Regularization-Images-person_ddim.git
+mkdir -p regularization_images/person_ddim
+mv -v Stable-Diffusion-Regularization-Images-person_ddim/person_ddim/* regularization_images/person_ddim/   
 python "main.py" \
  --base configs/stable-diffusion/v1-finetune_unfrozen.yaml \
  -t \
  --actual_resume "enki.ckpt" \
  --reg_data_root "/workspace/Dreambooth-Stable-Diffusion/regularization_images/person_ddim" \
- -n "dan" \
+ -n "alina" \
  --gpus 0, \
  --data_root "/workspace/Dreambooth-Stable-Diffusion/training_samples" \
- --max_training_steps 2800 \
- --class_word "style" \
- --token "dan" \
+ --max_training_steps 2000 \
+ --class_word "girl" \
+ --token "alina" \
  --no-test
