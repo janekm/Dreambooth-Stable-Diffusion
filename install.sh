08f60a037945886f893ca59c13f30aea4e3f6ffe
Janek Mann <janek.mann@knaufenergy.com>
2022-10-10 12:16:20 +0100

    update install

--- install.sh
+++ install.sh
@@ -9,15 +9,16 @@ cd /workspace
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
+
+cd /workspace
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git webui
+
 aws s3 cp s3://sd-models-1/wlop.ckpt models/Stable-diffusion/
 
 curl "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz" -o "ngrok-v3-stable-linux-amd64.tgz"
 tar xfvz ngrok-v3-stable-linux-amd64.tgz
 ./ngrok config add-authtoken ${NGROK_TOKEN}
 
-cd /workspace
-
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git webui
 
 cd /workspace/webui
 mv ../stable-diffusion-webui/repositories .
