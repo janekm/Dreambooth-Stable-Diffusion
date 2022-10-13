86963893bbfa6b731e915c8b004266eee9b22a19
Janek Mann <janek.mann@knaufenergy.com>
2022-10-13 16:45:37 +0100

    gunslinger

--- run-training.sh
+++ run-training.sh
@@ -1,12 +1,12 @@
 python "main.py" \
  --base configs/stable-diffusion/v1-finetune_unfrozen.yaml \
  -t \
- --actual_resume "wlop.ckpt" \
+ --actual_resume "guweiz1.ckpt" \
  --reg_data_root "/workspace/Dreambooth-Stable-Diffusion/regularization_images/person_ddim" \
- -n "testino" \
+ -n "gunslinger" \
  --gpus 0, \
  --data_root "/workspace/Dreambooth-Stable-Diffusion/training_samples" \
- --max_training_steps 1540 \
+ --max_training_steps 1818 \
  --class_word "style" \
- --token "testino" \
+ --token "gunslinger" \
  --no-test
