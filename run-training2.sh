34bbff1777da908fed7494d9d1573705f8f7f2d5
Janek Mann <janek.mann@knaufenergy.com>
2022-11-07 15:11:53 +0000

    update models

--- run-training2.sh
+++ run-training2.sh
@@ -1,11 +1,11 @@
 python "main.py" \
  --base configs/stable-diffusion/v1-finetune_unfrozen.yaml \
  -t \
- --actual_resume "rw-v1-5-full.ckpt" \
+ --actual_resume "par_15_a.ckpt" \
  --reg_data_root "/workspace/Dreambooth-Stable-Diffusion/woman_unsplash_3500" \
- -n "par_15_a" \
+ -n "lalisa" \
  --gpus 0, \
- --data_root "/workspace/Dreambooth-Stable-Diffusion/train3" \
- --max_training_steps 30000 \
+ --data_root "/workspace/Dreambooth-Stable-Diffusion/train6" \
+ --max_training_steps 7373 \
  --no-test \
  --token "notoken"
